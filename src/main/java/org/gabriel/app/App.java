8d5ca2611d3e5f6273781e4191153136c7c40036
Da0hn <vegancraft13@gmail.com>
2020-07-30 16:30:42 -0400

    Criada estrutura inicial do projeto

---
+++ src/main/java/org/gabriel/app/App.java
@@ -0,0 +1,11 @@
+package org.gabriel.app;
+
+/**
+ * @author daohn on 30/07/2020
+ * @project ExercicioMapeamentoJPA
+ */
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/gabriel/model/Cliente.java
@@ -0,0 +1,7 @@
+package org.gabriel.model;
+
+/**
+ * @author daohn on 30/07/2020
+ * @project ExercicioMapeamentoJPA
+ */
+public class Cliente {}
